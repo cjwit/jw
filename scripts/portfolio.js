dd49c4dd1c675f1879c31f728d32f0e2d2f8c4e5
cjwit <chris.witulski@gmail.com>
2016-04-12 21:48:46 -0400

    added files

---
+++ scripts/portfolio.js
@@ -0,0 +1,85 @@
+var videos = [
+	{
+		name: "This Little Babe",
+		img: 'images/littlebabe.png',
+		description: "Women's Chamber Choir at the 2015 Holiday Concert.",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/x4A49uBAsY4'
+	},
+	{
+		name: "Mama Who Bore Me",
+		img: 'images/mamawhoboreme.png',
+		description: "Select Choir women at the Fall 2015 Cabaret.",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/cr39ptnHuyA'
+	},
+	{
+		name: "Goin' on a Picnic",
+		img: 'images/stepsisters.png',	// REPLACE
+		description: "Our BRAVO! campers' opening number after one day of rehearsal 2015.",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/Gqgdyn6wg7E'
+	},
+	{
+		name: "Follow That Star",
+		img: 'images/followthestar.png',
+		description: "Concert Choir at the 2015 Holiday Concert.",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/qP8_-j8ESZU'
+	},
+	{
+		name: "Stepsisters Lament",
+		img: 'images/stepsisters.png',
+		description: "Our ENCORE! seventh through twelfth grade female campers 2015",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/8PjUiWob88M'
+	},
+	{
+		name: "I Wish",
+		img: 'images/iwish.png',
+		description: "Select Choir at the Fall 2014 Cabaret",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/MYMNQT5qYzk'
+	},
+	{
+		name: "Do Re Mi",
+		img: 'images/doremi.png',
+		description: "The Sound of Music Spring 2014",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/S4siRq4zcpk'
+	}
+]
+
+var studentprojects = [
+	{
+		name: "Goin' on a Picnic",
+		img: 'images/doremi.png',
+		description: "Our BRAVO! campers' opening number after one day of rehearsal 2015.",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/Gqgdyn6wg7E'
+	},
+	{
+		name: "Follow That Star",
+		img: 'images/doremi.png',
+		description: "Concert Choir at the 2015 Holiday Concert.",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/Gqgdyn6wg7E'
+	}
+]
+
+var documents = [
+	{
+		name: "Goin' on a Picnic",
+		img: 'images/doremi.png',
+		description: "Our BRAVO! campers' opening number after one day of rehearsal 2015.",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/Gqgdyn6wg7E'
+	},
+	{
+		name: "Follow That Star",
+		img: 'images/doremi.png',
+		description: "Concert Choir at the 2015 Holiday Concert.",
+		type: 'youtube',
+		link: 'https://www.youtube.com/embed/Gqgdyn6wg7E'
+	}
+]
